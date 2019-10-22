2eb576051c8c5201aa012d2728fdd5203bd7de26
live163 <live166@naver.com>
2019-10-23 06:27:44 +0900

    Add files via upload

---
+++ js/profile.js
@@ -0,0 +1,376 @@
+$(function(){
+    $(".name1").on({
+        "mouseover" : function() {
+            $("#pro1").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro1").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name2").on({
+        "mouseover" : function() {
+            $("#pro2").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro2").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name3").on({
+        "mouseover" : function() {
+            $("#pro3").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro3").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name4").on({
+        "mouseover" : function() {
+            $("#pro4").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro4").hide();
+            $("#mou").show();
+        }
+    });
+    
+    $(".name4").on({
+        "mouseover" : function() {
+            $("#pro4").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro4").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name5").on({
+        "mouseover" : function() {
+            $("#pro5").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro5").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name6").on({
+        "mouseover" : function() {
+            $("#pro6").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro6").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name7").on({
+        "mouseover" : function() {
+            $("#pro7").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro7").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name8").on({
+        "mouseover" : function() {
+            $("#pro8").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro8").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name9").on({
+        "mouseover" : function() {
+            $("#pro9").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro9").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name10").on({
+        "mouseover" : function() {
+            $("#pro10").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro10").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name11").on({
+        "mouseover" : function() {
+            $("#pro11").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro11").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name12").on({
+        "mouseover" : function() {
+            $("#pro12").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro12").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name13").on({
+        "mouseover" : function() {
+            $("#pro13").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro13").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name14").on({
+        "mouseover" : function() {
+            $("#pro14").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro14").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name15").on({
+        "mouseover" : function() {
+            $("#pro15").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro15").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name16").on({
+        "mouseover" : function() {
+            $("#pro16").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro16").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name17").on({
+        "mouseover" : function() {
+            $("#pro17").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro17").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name18").on({
+        "mouseover" : function() {
+            $("#pro18").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro18").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name19").on({
+        "mouseover" : function() {
+            $("#pro19").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro19").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name20").on({
+        "mouseover" : function() {
+            $("#pro20").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro20").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name21").on({
+        "mouseover" : function() {
+            $("#pro21").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro21").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name22").on({
+        "mouseover" : function() {
+            $("#pro22").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro22").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name23").on({
+        "mouseover" : function() {
+            $("#pro23").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro23").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name24").on({
+        "mouseover" : function() {
+            $("#pro24").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro24").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name25").on({
+        "mouseover" : function() {
+            $("#pro25").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro25").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name26").on({
+        "mouseover" : function() {
+            $("#pro26").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro26").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name27").on({
+        "mouseover" : function() {
+            $("#pro27").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro27").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name28").on({
+        "mouseover" : function() {
+            $("#pro28").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro28").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name29").on({
+        "mouseover" : function() {
+            $("#pro29").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro29").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name30").on({
+        "mouseover" : function() {
+            $("#pro30").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro30").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name31").on({
+        "mouseover" : function() {
+            $("#pro31").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro31").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name32").on({
+        "mouseover" : function() {
+            $("#pro32").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro32").hide();
+            $("#mou").show();
+        }
+    });
+
+    $(".name33").on({
+        "mouseover" : function() {
+            $("#pro33").fadeIn(10);
+            $("#mou").hide();
+        },
+        "mouseout" : function() {
+            $("#pro33").hide();
+            $("#mou").show();
+        }
+    });
+
+    });
