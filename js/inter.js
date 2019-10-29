79bd77b09ff58e37773a113b0805144f81fe01af
live163 <live166@naver.com>
2019-10-30 02:19:51 +0900

    Add files via upload

---
+++ js/inter.js
@@ -0,0 +1,99 @@
+$(function(){
+    $("#more1").on("click", function() {
+        $("#slide1").slideDown(500);
+    });
+    $("#slide1").on("click", function() {
+        $("#slide1").slideUp(500);
+    });
+
+    $("#more2").on("click", function() {
+        $("#slide2").slideDown(500);
+    });
+    $("#slide2").on("click", function() {
+        $("#slide2").slideUp(500);
+    });
+
+    $("#more3").on("click", function() {
+        $("#slide3").slideDown(500);
+    });
+    $("#slide3").on("click", function() {
+        $("#slide3").slideUp(500);
+    });
+
+    $("#more4").on("click", function() {
+        $("#slide4").slideDown(500);
+    });
+    $("#slide4").on("click", function() {
+        $("#slide4").slideUp(500);
+    });
+
+    $("#more5").on("click", function() {
+        $("#slide5").slideDown(500);
+    });
+    $("#slide5").on("click", function() {
+        $("#slide5").slideUp(500);
+    });
+
+    $("#more6").on("click", function() {
+        $("#slide6").slideDown(500);
+    });
+    $("#slide6").on("click", function() {
+        $("#slide6").slideUp(500);
+    });
+
+    $("#more7").on("click", function() {
+        $("#slide7").slideDown(500);
+    });
+    $("#slide7").on("click", function() {
+        $("#slide7").slideUp(500);
+    });
+
+    $("#more8").on("click", function() {
+        $("#slide8").slideDown(500);
+    });
+    $("#slide8").on("click", function() {
+        $("#slide8").slideUp(500);
+    });
+
+    $("#more9").on("click", function() {
+        $("#slide9").slideDown(500);
+    });
+    $("#slide9").on("click", function() {
+        $("#slide9").slideUp(500);
+    });
+
+    $("#more10").on("click", function() {
+        $("#slide10").slideDown(500);
+    });
+    $("#slide10").on("click", function() {
+        $("#slide10").slideUp(500);
+    });
+
+    $("#more11").on("click", function() {
+        $("#slide11").slideDown(500);
+    });
+    $("#slide11").on("click", function() {
+        $("#slide11").slideUp(500);
+    });
+
+    $("#more12").on("click", function() {
+        $("#slide12").slideDown(500);
+    });
+    $("#slide12").on("click", function() {
+        $("#slide12").slideUp(500);
+    });
+
+    $("#more13").on("click", function() {
+        $("#slide13").slideDown(500);
+    });
+    $("#slide13").on("click", function() {
+        $("#slide13").slideUp(500);
+    });
+
+    $("#more14").on("click", function() {
+        $("#slide14").slideDown(500);
+    });
+    $("#slide14").on("click", function() {
+        $("#slide14").slideUp(500);
+    });
+});
